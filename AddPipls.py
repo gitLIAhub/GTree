1474709578d93832d89abd91bbd72461ac561927
gitLIAhub <ilya-lipatov@mail.ru>
2015-03-05 03:00:48 +0300

    initial projet version

---
+++ AddPipls.py
@@ -0,0 +1,81 @@
+import os, sys                          # directory
+import shutil                           # copy files
+from numpy import *
+import winsound
+
+import numpy
+
+#from GenTree import Che
+import GenTree
+import imp
+imp.reload(GenTree)
+
+def Add(piplArr):
+    #piplArr=[]
+    Ilya=GenTree.Che("Lipatov", "Ilya", "Alexandrovich", piplArr)
+
+    Alex=GenTree.Che("Lipatov", "Alexandr", "Leonidovich", piplArr)
+    Nat=GenTree.Che("Lipatova", "Nataliya", "Evgenjevna", piplArr)
+
+    Evg=GenTree.Che("Bukatin", "Evgeniy", "Matveevich", piplArr)
+    Gala=GenTree.Che("Bukatina", "Galina", "Ivanovna", piplArr)
+
+    Leonid=GenTree.Che("Lipatov", "Leonid", "Sergeevich", piplArr)
+    Valja=GenTree.Che("Lipatova", "Valentina", "Ivanovna", piplArr)
+
+    SergLips=GenTree.Che("Lipatov", "Sergey", "Leonidovich", piplArr)
+    Sveta=GenTree.Che("Lipatova", "Svetlana", "Vadimovna", piplArr)
+
+    SergBar=GenTree.Che("Baranov", "Sergey", "Hzhzhz", piplArr)
+    Lena=GenTree.Che("Baranova", "Elena", "Evgenjevna", piplArr)
+    Nastja=GenTree.Che("Baranova", "Anastasija", "Sergeevna", piplArr)
+    
+    SergPra=GenTree.Che("Lipatov", "Sergey", "Hzhzhz", piplArr)#Deda Otec
+    Dusja=GenTree.Che("Lipatova", "Evdokija", "Hzhzhz", piplArr)#Deda Mama
+    SashaDedBro=GenTree.Che("Lipatov", "Alexandr", "Sergeevich", piplArr)#Deda Brat
+
+    IvanPra=GenTree.Che("NotLip", "Ivan", "Hzhzhz", piplArr)#Bbshk Otec
+    KlavaPra=GenTree.Che("NotLip", "Klavdija", "Hzhzhz", piplArr)#Bbshk Mama
+    
+    ZoyaS=GenTree.Che("NotSokolova", "Zoya", "Ivanovna", piplArr)
+    ZinaS=GenTree.Che("Kosheleva", "Zinaida", "Ivanovns", piplArr)
+    LubaS=GenTree.Che("NotSokolova", "Lubov'", "Ivanovna", piplArr)
+    DLenjaK=GenTree.Che("Koshelev", "Leonid", "Georgievich", piplArr)
+    LeshaK=GenTree.Che("Koshelev", "Alexey", "Leonidovich", piplArr)
+    NatashaK=GenTree.Che("Kosheleva", "Natalyja", "Hzhzhz", piplArr)
+    LeonidBrat=GenTree.Che("Koshelev", "Leonid", "Alexeevich", piplArr)
+    DSlavaS=GenTree.Che("Sokolov", "Vjacheslav'", "Ivanovich", piplArr)
+    #=GenTree.Che("", "", "", piplArr)
+
+    #Ilya.AllOut()
+
+    Ilya.setParents(Alex, Nat)
+    Nat.setParents(Evg, Gala)
+    Alex.setParents(Leonid, Valja)
+    Lena.setParents(Evg, Gala)
+    Nastja.setParents(SergBar, Lena)
+    SergLips.setParents(Leonid, Valja)
+    Leonid.setParents(SergPra, Dusja)
+    SashaDedBro.setParents(SergPra, Dusja)
+    #Not only Koshelevi
+    Valja.setParents(IvanPra, KlavaPra)
+    ZinaS.setParents(IvanPra, KlavaPra)
+    ZoyaS.setParents(IvanPra, KlavaPra)
+    LubaS.setParents(IvanPra, KlavaPra)
+    DSlavaS.setParents(IvanPra, KlavaPra)
+    LeshaK.setParents(DLenjaK, ZinaS)
+    LeonidBrat.setParents(LeshaK, NatashaK)
+    # setParents(, )
+        
+    SergLips.marry(Sveta, 1989)
+    Alex.marry(Nat, 1992)
+    Leonid.marry(Valja, 1963)
+    Evg.marry(Gala)
+    SergBar.marry(Lena, 2004)
+    DLenjaK.marry(ZinaS)
+    LeshaK.marry(NatashaK)
+    #.marry()
+
+    return piplArr
+
+    
---
+++ GenTree.py
@@ -0,0 +1,162 @@
+import os, sys                          # directory
+import shutil                           # copy files
+from numpy import *
+import winsound
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import art3d
+
+import Image, ImageEnhance
+import numpy
+
+import AddPipls
+# !!!Vot blin kruto!! FckCKKnSHt!
+import imp
+imp.reload(AddPipls)
+
+
+class AllFamily(object):
+    AllId=[]
+    family=[]
+    def getNewId(self):
+        a=1
+        while (self.AllId.count(a)>0):
+            a+=1
+        self.AllId.append(a)        
+        return a
+    def setGenerationLvls(self):
+        maxlvl=-500
+        for pipl in self.family:
+            pass
+    def getPiplFromId(self, Id):
+        print "getPiplFromId_Id=", Id
+        for pipl in self.family:
+            if (pipl._Id==Id):
+                return pipl
+        print "No pipl with Id=", Id
+        return self.family[0]
+    
+def deligate(family, toPiplId, toRecLev):
+    if not(toPiplId==-1):
+        if ((family.getPiplFromId(toPiplId)).isGLevSet==False):
+            setGenerLev(family, family.getPiplFromId(toPiplId), toRecLev)
+        else:
+            print "Else Else!!"
+            
+def setGenerLev(family, self, recLev):
+    self.setGLev(recLev)
+    self.isGLevSet=True
+    deligate(family, self.DadId, recLev+1)
+    deligate(family, self.MomId, recLev+1)
+    for piplId in self.Childs:
+        deligate(family, piplId, recLev-1)
+    deligate(family, self.Spouse, recLev)
+    """if not(self.DadId==-1):
+        if ((family.getPiplFromId(self.DadId)).isGLevSet==False):
+            setGenerLev(family, family.getPiplFromId(self.DadId), recLev+1)
+        else:
+            print "Else Else!!"
+     """       
+    
+    
+class Che(object):
+    Name, LastName, Otche = "","",""
+    isGLevSet=False
+    _Id=-1
+    DadId=-1
+    MomId=-1
+    Spouse=-1       #wedding
+    DateWedding=-1
+    NChilds=0       #childs    
+    GenerationLevel=-500
+    __EndOfRepr__="\n"
+
+    def setGLev(self, recLev):
+        self.GenerationLevel=recLev
+        
+    def __init__(self, LastNamein, Namein, Otchein, Family):
+        self.Name=Namein
+        self.LastName=LastNamein
+        self.Otche=Otchein
+        self._Id=Family.getNewId()
+        self.Childs=[]
+        Family.family.append(self)
+        
+    def marry(self, spouse, date='-1'):
+        self.Spouse=spouse._Id
+        spouse.Spouse=self._Id
+        self.DateWedding=date
+        spouse.DateWedding=date
+        
+    def addChild(self, chld):
+        self.NChilds+=1
+        self.Childs.append(chld._Id)
+        
+    def setDad(self, dad):
+        self.DadId=dad._Id
+        dad.addChild(self)
+    def setMom(self, mom):
+        self.MomId=mom._Id
+        mom.addChild(self)
+    def setParents(self, dad, mom):
+        self.setMom(mom)
+        self.setDad(dad)
+        dad.marry(mom)
+    
+    
+    def __str__(self):
+        return "%sId=%2.0f, %s %s %s" % (self.__EndOfRepr__,  self._Id, self.LastName, self.Name, self.Otche)
+    def __repr__(self): 
+        return "%sId=%2.0f, %s %s %s" % (self.__EndOfRepr__,  self._Id, self.LastName, self.Name, self.Otche)
+    def AllOut(self):
+        maxLenOfAttr=0
+        for attr in dir(self):
+            if not((attr[1]=='_')or (type(self.__getattribute__(attr))==type(self.__getattribute__("setDad")))):
+                if len(attr)>maxLenOfAttr:
+                    maxLenOfAttr=len(attr)
+        def separate(s="  "):
+            print s
+        def printList(lst, seprr="  "):
+            if (type(lst)==type("abc")):
+                attr=lst
+                print ("{:"+str(maxLenOfAttr)+"}: {}").format(attr, self.__getattribute__(attr))
+            else:
+                for attr in lst:
+                    print ("{:"+str(maxLenOfAttr)+"}: {}").format(attr, self.__getattribute__(attr))
+            separate(seprr)
+        printList(('_Id', 'LastName', 'Name', 'Otche'))
+        printList(('GenerationLevel', 'isGLevSet'))
+        printList(('DadId', 'MomId')," - "*10)
+        printList(('Spouse', 'DateWedding'))
+        printList(('NChilds', 'Childs'))
+
+    def __eq__(self, other):
+        isEqual = True
+        for attr in ('LastName', 'Name', 'Otche', 'DadId', 'MomId'):
+            isEqual = isEqual and (self.__getattribute__(attr)==other.__getattribute__(attr))
+        return isEqual
+
+def PrintAll(piplArr):
+    print "_"*50
+    for pipl in piplArr:
+        print "_"*50
+        pipl.AllOut()
+    print "_"*50
+
+if __name__ == '__main__':
+    PATHthisFULL=os.path.abspath(__file__)  # full name of file
+    PATHthisROOT=os.path.dirname(PATHthisFULL)  # name of directory of this file
+    
+    Lips=AllFamily()
+    PrintAll(Lips.family)
+    Lips=AddPipls.Add(Lips)
+    print "Lips family"
+    PrintAll(Lips.family)
+
+    print "Ilya's family:", Lips.family
+
+
+    
